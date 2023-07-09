4cb2c6be44549bd98315adc97cb65c2e5eea2158
Camo <roberts@nallus.com>
2023-07-09 15:21:12 +0300

    Update Discord.py

--- messengers/Discord.py
+++ messengers/Discord.py
@@ -4,12 +4,12 @@ from urllib.request import Request, urlopen
 import json
 
 class Discord:
-    def __init__(self, webhook, botname="April O'Neil", avatar="https://i.pinimg.com/originals/87/67/11/876711e56a0ef942cbb2f15844235f2e.jpg"):
+    def __init__(self, webhook:str, botname:str="April O'Neil", avatar:str="https://i.pinimg.com/originals/87/67/11/876711e56a0ef942cbb2f15844235f2e.jpg"):
         self.webhook, self.botname, self.avatar = webhook, botname, avatar
         if not webhook.startswith("https://discord.com/api/webhooks/"):
             raise ValueError("Invalid webhook URL")
 
-    def send_message(self, message):
+    def send_message(self, message:str):
         data = json.dumps({"username": self.botname, "avatar_url": self.avatar, "content": message}).encode('utf-8')
         req = Request(self.webhook, data, headers={'Content-Type': 'application/json','User-Agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
         urlopen(req).read()
